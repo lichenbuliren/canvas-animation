69b4f3543d0817e438285e75e5446ab63ebf715d
lichenbuliren <617106268@qq.com>
2017-03-01 14:41:34 +0800

    update

--- src/util/particle3d.js
+++ src/util/particle3d.js
@@ -4,23 +4,60 @@ export class Particle3d {
     this.y = opt.y || 0;
     this.z = opt.z || 0;
     this.color = opt.color || '#fff';
-    this.radius = opt.radius || 2;
+    // 三维球体半径
+    this.ballR = opt.ballR;
+    // 粒子半径
+    this.r = opt.r || 2;
+    this.vpX = opt.vpX;
+    this.vpY = opt.vpY;
+
+    // 三维坐标投影二维坐标
+    this.x2 = undefined;
+    this.y2 = undefined;
+    this.r2 = undefined;
   }
 
   translate2d(perspective) {
-    let scale = 1/(1 + this.z/perspective);
-    this.x = (this.x - this.centerX) * scale + centerX;
-    this.y = (this.y - this.centerY)* scale + centerY;
-    this.radius = this.radius * scale;
+    let scale = 1 / (1 + this.z / perspective);
+    this.x2 = this.x * scale;
+    this.y2 = this.y * scale;
+    this.r2 = this.r * scale;
+  }
+
+  rotateY(angle) {
+    var cosy = Math.cos(angle);
+    var siny = Math.sin(angle);
+    var x1 = this.x * cosy + this.z * siny;
+    var z1 = this.z * cosy - this.x * siny;
+    this.x = x1;
+    this.z = z1;
+  }
+
+  rotateX(angle) {
+    var cosx = Math.cos(angle);
+    var sinx = Math.sin(angle);
+    var z1 = this.z * cosx - this.y * sinx;
+    var y1 = this.y * sinx + this.z * cosx;
+    this.y = y1;
+    this.z = z1;
+  }
+
+  rotateZ(angle) {
+    var cosz = Math.cos(angle);
+    var sinz = Math.sin(angle);
+    var x1 = this.x * cosz - y * sinz;
+    var y1 = this.y * cosz + x * sinz;
+    this.x = x1;
+    this.y = y1;
   }
 
   draw(context, perspective) {
-    // this.translate2d(perspective);
     context.save();
-    context.translate(this.xpos, this.ypos);
+    context.translate(this.vpX, this.vpY);
+    this.translate2d(perspective);
     context.fillStyle = this.color;
     context.beginPath();
-    context.arc(0, 0, this.radius, 0, Math.PI * 2, false);
+    context.arc(this.x2, this.y2, this.r2, 0, Math.PI * 2, true);
     context.closePath();
     context.fill();
     context.restore();
--- src/view/sphere/index.js
+++ src/view/sphere/index.js
@@ -1,24 +1,29 @@
-import { util, Particle3d } from '../../util/';
+import {
+  util,
+  Particle3d
+} from '../../util/';
 require('../index.css');
 
 window.onload = function () {
   const canvasBody = document.getElementById("canvas");
   const context = canvasBody.getContext("2d");
-  let particles = [], canvasWidth, canvasHeight, centerX, centerY;
-  let delay = 200, timer;
+  let particles = [],
+    canvasWidth, canvasHeight, centerX, centerY;
+  let delay = 200,
+    timer;
   canvasWidth = canvasBody.width = window.innerWidth;
   canvasHeight = canvasBody.height = window.innerHeight;
   canvasWidth = canvasBody.width, canvasHeight = canvasBody.height;
-  centerX = canvasWidth/2, centerY = canvasHeight/2;
+  centerX = canvasWidth / 2, centerY = canvasHeight / 2;
   const config = {
     particleColor: '#fff',
     lineColor: '#fff',
     particleAmount: 100,
     defaultRadius: 4,
-    perspective: 400, //视距
-    sphereRadius: 200 //球体半径
+    perspective: 1200, //视距
+    sphereRadius: 400 //球体半径
   };
-  
+
   canvasBody.style.backgroundColor = '#000';
   setup();
 
@@ -34,53 +39,51 @@ window.onload = function () {
   }
 
   function setup() {
-    for (var i = 0; i < config.particleAmount; i++) {
-      particles.push(new Particle3d({
-        x: util.getRandom(0, canvasWidth),
-        y: util.getRandom(0, canvasHeight),
-        z: util.getRandom(-2000, config.perspective),
-        centerX: centerX,
-        centerY: centerY,
-        radius: config.defaultRadius,
-        color: config.particleColor,
-        perspective: config.perspective
-      }));
-      
+    // 设置球面粒子坐标, angleY 从 0 到 180°，angleX 从 0 到 360°，分成 10 等分
+    // x 坐标：x = r*sin(angleY)*cos(angleX);
+    // y 坐标：y = r*cos(angleY);
+    // z 坐标：z = r*sin(angleY)*sin(angleX);
+    var angleY = 0;
+    for (var i = 0; i < 18; i++) { // y 轴 10 等分
+      var angleX = 0;
+      for (var j = 0; j < 36; j++) { // x 轴 30 等分
+        var x = config.sphereRadius * Math.sin(angleY) * Math.cos(angleX);
+        var y = config.sphereRadius * Math.cos(angleY);
+        var z = config.sphereRadius * Math.sin(angleY) * Math.sin(angleX);
+        // 验证公式是否正确 x² + y² + z² = r²；
+        console.log(Math.round((Math.sqrt(x * x + y * y + z * z))));
+        particles.push(new Particle3d({
+          x,
+          y,
+          z,
+          r: 4,
+          ballR: config.sphereRadius,
+          vpX: centerX,
+          vpY: centerY,
+          perspective: config.perspective
+        }));
+        angleX += 10;
+      }
+      angleY += 10;
     }
+    console.log(angleY, angleX);
     requestAnimationFrame(loop);
   }
+  particles.sort(function(a, b) {
+    return b.z - a.z;
+  });
+
   console.log(particles);
 
+  var rotateY = -0.001;
   function loop() {
+    // rotateY = 0.0001;
     requestAnimationFrame(loop);
     context.clearRect(0, 0, canvasWidth, canvasHeight);
     for (var i = 0; i < particles.length; i++) {
+      particles[i].rotateY(rotateY);
       particles[i].draw(context, config.perspective);
     }
-
-    for (let i = 0; i < particles.length; i++) {
-      linkPoints(particles[i], particles);
-    }
-  }
-
-  function linkPoints(p1, hubs) {
-    for (var i = 0; i < hubs.length; i++) {
-      let distance = checkDistance(p1, hubs[i]);
-      let opacity = 1 - distance / config.linkRadius;
-      if (opacity > 0) {
-        context.lineWidth = .2;
-        context.strokeStyle = config.particleColor;
-        context.beginPath();
-        context.moveTo(p1.x, p1.y);
-        context.lineTo(hubs[i].x, hubs[i].y);
-        context.closePath();
-        context.stroke();
-      }
-    }
-  }
-
-  function checkDistance(point1, point2) {
-    return Math.sqrt(Math.pow(point2.x - point1.x, 2) + Math.pow(point2.y - point1.y, 2));
   }
 
   function resizeReset() {
